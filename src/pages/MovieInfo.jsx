4fcd3d04e898274aa6b937c5128731d9ef6345e3
Jiří Strnadel <Juzba88@gmail.com>
2025-03-03 23:35:36 +0100

    done

--- src/pages/MovieInfo.jsx
+++ src/pages/MovieInfo.jsx
@@ -1,64 +1,67 @@
-import { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { firestoreDB } from '../firebase/config';
-import { collection, onSnapshot } from 'firebase/firestore';
-import Loading from './page_components/Loading';
-import './Page_scss/MovieInfo.scss';
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { firestoreDB } from "../firebase/config";
+import { collection, onSnapshot } from "firebase/firestore";
+import Loading from "./page_components/Loading";
+import "./Page_scss/MovieInfo.scss";
 
 const MovieInfo = () => {
-    const [movieInfo, setMovieInfo] = useState(false);
-    const [error, setError] = useState('');
-    console.log(movieInfo);
+	const [movieInfo, setMovieInfo] = useState(false);
+	const [error, setError] = useState("");
+	console.log(movieInfo);
 
-    const { id } = useParams();
+	const { id } = useParams();
 
-    useEffect(() => {
-        const unsub = onSnapshot(
-            collection(firestoreDB, 'movies_two'),
-            (snapshot) => {
-                const oneMovie = snapshot.docs.find((oneDoc) => {
-                    return oneDoc.id == id;
-                });
-                if (oneMovie) {
-                    setMovieInfo(oneMovie.data());
-                    setError('');
-                } else {
-                    setError('Film Nenalezen.');
-                    setMovieInfo(false);
-                }
-            },
-            (err) => setError(err.message)
-        );
+	useEffect(() => {
+		const unsub = onSnapshot(
+			collection(firestoreDB, "movies_two"),
+			(snapshot) => {
+				const oneMovie = snapshot.docs.find((oneDoc) => {
+					return oneDoc.id == id;
+				});
+				if (oneMovie) {
+					setMovieInfo(oneMovie.data());
+					setError("");
+				} else {
+					setError("Film Nenalezen.");
+					setMovieInfo(false);
+				}
+			},
+			(err) => setError(err.message)
+		);
 
-        return () => unsub();
-    }, [id]);
+		return () => unsub();
+	}, [id]);
 
-    return (
-        <section className="movie-info">
-            <h1>Movie Info</h1>
-            {movieInfo && !error ? (
-                <div className="movie-card">
-                    <h2>{movieInfo.title}</h2>
-                    <table>
-                        <tr>
-                            <th className='left'>Dostupné od věku:</th>
-                            <th className='right'>{"+" + movieInfo.minage}</th>
-                        </tr>
-                        <tr>
-                            <th className='left'>Délka filmu:</th>
-                            <th className='right'>{movieInfo.time + " min"}</th>
-                        </tr>
-                    </table>
+	return (
+		<section className="movie-info">
+			<h1>Movie Info</h1>
+			{movieInfo && !error ? (
+				<div className="movie-card">
+					<h2>{movieInfo.title}</h2>
 
-                    {/* <p>{`Dostupné od věku: +${movieInfo.minage}.`}</p>
+					<table>
+						<thead>
+							<tr>
+								<th className="left">Dostupné od věku:</th>
+								<th className="right">{movieInfo.minage + " let"}</th>
+							</tr>
+							<tr>
+								<th className="left">Délka filmu:</th>
+								<th className="right">{movieInfo.time + " min"}</th>
+							</tr>
+						</thead>
+					</table>
+
+					{/* <p>{`Dostupné od věku: +${movieInfo.minage}.`}</p>
                     <p>{`Délka filmu: ${movieInfo.time}min.`}</p> */}
-                    <Link to={'/movies'}>Návrat na stránku Movies.</Link>
-                </div>
-            ) : (
-                <div>{error ? <p>{error}</p> : <Loading />}</div>
-            )}
-        </section>
-    );
+					<Link to={"/movies"}>Návrat na stránku Movies.</Link>
+				</div>
+			) : (
+				<div>{error ? <p>{error}</p> : <Loading />}</div>
+			)}
+		</section>
+	);
 };
 
 export default MovieInfo;
--- src/pages/page_components/Loading.jsx
+++ src/pages/page_components/Loading.jsx
@@ -1,14 +1,13 @@
-import './Loading.scss';
+import "./Loading.scss";
 
 const Loading = () => {
-    return (
-        <div className="loading">
-            <p>Loading</p>
-            <div className="load-box">
 
-            </div>
-        </div>
-    );
+	return (
+		<div className="loading" id="onLoad">
+			<p>Loading</p>
+			<div className="load-box"></div>
+		</div>
+	);
 };
 
 export default Loading;
